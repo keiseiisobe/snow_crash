3e9cfc86e84c36e067b37872d0d4934e84784e9d
keiseiisobe <keisei.isobe@gmail.com>
2026-09-12 14:35:32 +0900

    add command to strings.sh

--- level00/resources/strings.sh
+++ level00/resources/strings.sh
@@ -1,8 +1,23 @@
 #! /bin/bash
 
-echo "Running strings command ..."
-echo "Output ---------"
+if ! command -v sshpass
+then
+    echo -e "Install sshpass command before executing this shell script."
+else
+    USER="level00"
+    ADDRESS="localhost"
+    PORT="4242"
+    export SSHPASS="level00"
+    echo -e "\nExploiting level00 ssh server ..."
+    file_name=$(sshpass -e ssh -p "$PORT" "$USER"@"$ADDRESS" "find / -type f -user flag00 2> /dev/null")
+    echo -e "\nGot a binary file:"
+    echo -e $file_name
+    sshpass -e scp -P $PORT $USER@$ADDRESS:$file_name ./
+   
+    echo -e "\nRunning strings command to an extracted file ..."
+    echo -e "\n\n\n---------- Output ----------"
 
-strings ./hiro
+    strings ./hiro
 
-echo "--------"
+    echo -e "----------------------------"
+fi    
